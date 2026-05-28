26390bb93a13410c87161e76b3422c10672c501f
1BinMansour <abdullahh.i83m@gmail.com>
2026-05-29 02:26:45 +0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -6,7 +6,7 @@ import { createClient } from "@supabase/supabase-js";
 //  (اتبع الشرح في ملف SETUP.md خطوة بخطوة)
 // ════════════════════════════════════════════════════════════
 
-const SUPABASE_URL = "https://qyrvfjesatztqhlrhhcm.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_VA-fyTXrs1wu2vrbuQIdYg_-2pHtG8_";
+const SUPABASE_URL = "https://qyrvfjesatztqhlrhhcm.supabase.co"
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5cnZmamVzYXR6dHFobHJoaGNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5OTUyNzYsImV4cCI6MjA5NTU3MTI3Nn0.VP_fTU9lXODgTe4i7AsQDdGSZhbJHQi0o8P_mlzTMmg";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
